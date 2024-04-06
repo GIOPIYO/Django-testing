b368e481308414aedd6800de2a501dc2dd7b434d
Joseph <147633570+Joseph-0147@users.noreply.github.com>
2024-04-06 14:07:52 +0300

    Add files via upload

--- hello/urls.py
+++ hello/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path("contact/",views.contact, name="contact"),
     path("catalog/",views.catalog, name="catalog"),
     path("catalog1/",views.catalog1, name="catalog1"),
-    path("prodDetails/", views.prodDetails, name="prodDetails"),
     path("search/", views.search, name="search"),
-    path('FarmingEquipment/<str:product_name>/', views.prodDetails, name="prodDetails"),
+    path("product/<str:product_name>/", views.prodDetails, name="prodDetails"),
+    path("add_to_cart/<int:product_id>/", views.add_to_cart, name="add_to_cart"),
+    path("cart_summary/", views.cart_summary, name="cart_summary"),
+    path('checkout/', views.checkout, name='checkout'),
+    path('empty_cart/', views.empty_cart, name='empty_cart'),
 ]
